9252e7cbc4b4813aa79a84e893220e7d3cc5b661
rl3032 <runjielyu@gmail.com>
2023-08-29 15:06:52 -0700

    Update to recommend a list with provided name using KNN

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
+from src.load_data import load_data, get_song_index
+from src.model import train_knn_model, recommend_songs
 
-
-# Load the dataset
-data = pd.read_csv("data/data.csv")
-
-song_features_normalized = ['valence', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness']
-song_features_not_normalized = ['duration_ms', 'key', 'loudness', 'mode', 'tempo']
-
-all_features = song_features_normalized + song_features_not_normalized + ['decade', 'popularity']
-
-# set a good looking font
+# set the looking font
 st.markdown(
     """
     <style>
@@ -28,15 +19,32 @@ def main():
     st.markdown("Welcome to your personalized music recommender engine!\
                 You can input the name of a song and get a recommendation list.")
     
+    # Load dataset
+    data = load_data("data/data.csv")
+
+    song_features_normalized = ['valence', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'speechiness']
+    song_features_not_normalized = ['duration_ms', 'key', 'loudness', 'mode', 'tempo']
+
+    all_features = song_features_normalized + song_features_not_normalized + ['year', 'popularity']
+
+    X = data[all_features]
+
+    model = train_knn_model(X)
+
     # Add selections to the sidebar
     st.sidebar.markdown("### Number of Recommendations")
-    num_rec_songs = st.sidebar.slider("Select the number of songs in your list", 
-                                      5, 30, 10)
+    num_rec_songs = st.sidebar.slider("Select the number of songs in your list", 5, 30, 10)
     
 
     # Provide a text area for the user to enter a seed song
-    st.markdown("## Try to generate a recommendation list")
-    song_name = st.text_area("Enter the name of a song")
+    song_name = st.text_area("Enter a song name")
+    
+    if st.button("Recommend"):
+        song_index = get_song_index(song_name, data)
+        recommend_song_indices = recommend_songs(model, song_index, X, num_rec_songs)
+        recommend_song_list = data[['name', 'artists', 'year', 'popularity']].iloc[recommend_song_indices]
+        st.write(recommend_song_list)  
+    
 
 if __name__ == "__main__":
     main()
