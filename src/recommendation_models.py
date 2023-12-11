5ac44ea8def669b45cf1c52bfe8f014ca640ccd8
rl3032 <runjielyu@gmail.com>
2023-12-11 15:32:00 -0800

    Add the Item-Centered Model

--- src/recommendation_models.py
+++ src/recommendation_models.py
@@ -8,7 +8,7 @@ Model 1: K-Means Clustering
 Model 2: Content-Based Filtering
 """
 
-
+from collections import defaultdict
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import pandas as pd
@@ -52,36 +52,44 @@ def recommend_songs_keans(song_name, data, kmeans_model, scaler, features, top_n
     return recommendations[['name', 'artists', 'year']].head(top_n)
 
 
-def recommend_songs_cosine_similarity(song_name, data, similarity_matrix, top_n=10):
+def recommend_songs_content_based(song_name, data, scaler, features, top_n=10, popularity_weight=0.5):
     """
-    Recommend top n songs closest to the seed song based on cosine similarity.
+    Recommend top n songs based on a mix of cosine similarity and popularity.
 
     Args:
-        song_name: Name of the song to base recommendations on.
+        song_name: Name of the seed song to base recommendations on.
         data: Pandas DataFrame of song data.
-        similarity_matrix: Numpy array of cosine similarity scores.
+        scaler: StandardScaler object used for feature scaling.
+        features: List of features used in clustering.
         top_n: Number of top recommendations to return.
+        popularity_weight: Weight for popularity in the final score (0 to 1).
 
     Returns:
-        Pandas DataFrame of top n recommended songs closest to the seed song.
+        Pandas DataFrame of top n recommended songs.
     """
     if song_name not in data['name'].values:
-        return "Song not found in the dataset."
+        return "Seed song not found in the dataset."
 
-    # Find the index of the seed song
-    song_index = data[data['name'] == song_name].index[0]
+    # Find the feature vector of the seed song
+    song_features = scaler.transform(data[data['name'] == song_name][features])
 
-    # Get the similarity scores of the seed song with all other songs
-    song_similarities = similarity_matrix[song_index]
+    # Calculate similarity scores for all songs
+    similarities = cosine_similarity(song_features, scaler.transform(data[features]))
+    data['similarity'] = np.squeeze(similarities)
 
-    # Create a DataFrame of songs and their similarity scores
-    recommendations = pd.DataFrame({'name': data['name'], 'artists': data['artists'], 'year': data['year'], 'similarity': song_similarities})
+    # Normalize popularity
+    data['normalized_popularity'] = data['popularity'] / data['popularity'].max()
 
-    # Sort the songs based on similarity scores
-    recommendations = recommendations.sort_values(by='similarity', ascending=False)
+    # Combine similarity and popularity
+    data['score'] = (1 - popularity_weight) * data['similarity'] + popularity_weight * data['normalized_popularity']
+
+    # Sort the songs based on the combined score
+    recommendations = data.sort_values(by='score', ascending=False)
 
     # Exclude the seed song and return the top n songs
     recommendations = recommendations[recommendations['name'] != song_name]
-    return recommendations[['name', 'artists', 'year']].head(top_n)
+    return recommendations[['name', 'artists', 'year', 'score']].head(top_n)
+
+
     
     
