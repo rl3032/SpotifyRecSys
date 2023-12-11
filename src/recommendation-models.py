eb3cda0180b4df91e66b2e253255df84cd09c400
rl3032 <runjielyu@gmail.com>
2023-12-11 10:28:06 -0800

    Remove extra file and add basic rec model

---
+++ src/recommendation-models.py
@@ -0,0 +1,53 @@
+"""
+Recommendation Algorithm for Spotify Song data.
+
+Author: RJ
+Date: 12/09/2023
+
+Model 1: K-Means Clustering
+Model 2: Content-Based Filtering
+"""
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+def recommend_songs(song_name, data, kmeans_model, scaler, features, top_n=10):
+    """
+    Recommend top n songs closest to the seed song based on KMeans clustering.
+
+    Args:
+        song_name: Name of the song to base recommendations on.
+        data: Pandas DataFrame of song data.
+        kmeans_model: Trained KMeans model.
+        scaler: StandardScaler object used for feature scaling.
+        features: List of features used in clustering.
+        top_n: Number of top recommendations to return.
+
+    Returns:
+        Pandas DataFrame of top n recommended songs closest to the seed song.
+    """
+    if song_name not in data['name'].values:
+        return "Song not found in the dataset."
+
+    # Find the feature vector of the seed song
+    seed_song_features = scaler.transform(data[data['name'] == song_name][features])
+
+    # Predict the cluster for the seed song
+    song_cluster = kmeans_model.predict(seed_song_features)[0]
+
+    # Filter out the songs from the same cluster
+    cluster_songs = data[data['cluster'] == song_cluster]
+
+    # Calculate similarity scores for all songs in the same cluster
+    similarities = cosine_similarity(seed_song_features, scaler.transform(cluster_songs[features]))
+    cluster_songs['similarity'] = np.squeeze(similarities)
+
+    # Sort the songs based on similarity scores
+    recommendations = cluster_songs.sort_values(by='similarity', ascending=False)
+
+    # Exclude the seed song and return the top n songs
+    recommendations = recommendations[recommendations['name'] != song_name]
+    return recommendations[['name', 'artists', 'year']].head(top_n)
+
+
